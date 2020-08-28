55930f439b1eb9227b2f41aeed55d5ec105d43de
ZainRamzan <64620737+ZainRamzanGorsi@users.noreply.github.com>
2020-08-28 22:38:58 +0500

    Initial commit

---
+++ build/precache-manifest.56052ae954228f43d3856d7ad59901dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113876f6df0687fb84abc35d51486f99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb0cb64583f7eba40e2f",
+    "url": "/static/css/main.f877024d.chunk.css"
+  },
+  {
+    "revision": "ce3ae69b951701f0e040",
+    "url": "/static/js/2.9db34ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9db34ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0cb64583f7eba40e2f",
+    "url": "/static/js/main.0d143d6e.chunk.js"
+  },
+  {
+    "revision": "be04023da6f82a43f227",
+    "url": "/static/js/runtime-main.b7ed62f2.js"
+  }
+]);
